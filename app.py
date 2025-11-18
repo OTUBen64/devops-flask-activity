a884463540e07c7a0611570fc55feb388690133a
OTUBen64 <benjamin.walsh1@ontariotechu.net>
2025-11-17 23:01:09 -0500

    New endpoints for app and test_app

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -6,6 +5,11 @@ app = Flask(__name__)
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello, World!")
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json(force=True)
+    return jsonify(data), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
